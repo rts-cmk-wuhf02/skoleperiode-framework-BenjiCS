3acf871024e1472eb2b7eddd01e3fff650ae3f81
Benjamin Hansen <benjamin.hansen44@gmail.com>
2020-04-24 08:55:42 +0200

    *

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+/* import Pet from "./Pet"; */
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
+  /* return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       name: "luna",
-      animal: "dog",
-      breed: "Havanese",
+      animal: "Dog",
+      breed: "Pug",
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
       breed: "Cockatiel",
     }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]); */
+
+  return (
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+        {/*       <Pet name="luna" animal="Dog" breed="Pug" />
+      <Pet name="Pepper" animal="Bird" breed="Labrador" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
+      </div>
+    </React.StrictMode>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
