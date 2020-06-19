1ee40847c6b365367ba46e5e581e2c572bc500fb
Benjamin Hansen <benjamin.hansen44@gmail.com>
2020-06-19 20:50:17 +0200

    *

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
-/* import Pet from "./Pet"; */
-import SearchParams from "./SearchParams";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./searchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  /* return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "luna",
-      animal: "Dog",
-      breed: "Pug",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]); */
-
+  const themeHook = useState("darkblue");
   return (
     <React.StrictMode>
-      <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
-        {/*       <Pet name="luna" animal="Dog" breed="Pug" />
-      <Pet name="Pepper" animal="Bird" breed="Labrador" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adobt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,45 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect, useContext } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("Seatlle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+  const [theme] = useContext(ThemeContext);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreeds, setBreeds]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
@@ -22,8 +51,9 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
